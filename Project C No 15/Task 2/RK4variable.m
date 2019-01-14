d7ebb6bd55ee77b436bef85fe820cd07d8b255ce
Wiktor Łazarski <wjlazarski@gmail.com>
2019-01-15 00:08:59 +0100

    RK4 with variable step size fixed

--- Project C No 15/Task 2/RK4variable.m
+++ Project C No 15/Task 2/RK4variable.m
@@ -1,16 +1,17 @@
-function [x1values, x2values, errors] = RK4variable(fun1, fun2, x1, x2, h, epsr, epsa)
+function [x1values, x2values, errors, steps] = RK4variable(fun1, fun2, x1, x2, h, epsr, epsa)
 % x1 - first value of x1
 % x2 - first value of x2
 % h - step
 % epsr - relative epsilon
 % epsa - absolute epsilon
-i = 0;
+i = 1;
 a = 0;
 
+x1values(1) = x1;
+x2values(1) = x2;
+steps(i) = h;
+
 while (a < 20)
-    x1values(i + 1) = x1;
-    x2values(i + 1) = x2;
-    
     %next points
     [k1, k2] = computeKs(fun1, fun2, x1, x2, h);
     
@@ -20,50 +21,56 @@ while (a < 20)
     x1 = x1 + h * (k1(1) + 2 * k1(2) + 2 * k1(3) + k1(4)) / 6;
     x2 = x2 + h * (k2(1) + 2 * k2(2) + 2 * k2(3) + k2(4)) / 6;
     
+    x1values(i + 1) = x1;
+    x2values(i + 1) = x2;
+    
     %first half-step
     h = 0.5 * h;
-    x1 = tempx1;
-    x2 = tempx2;
     
     [k1, k2] = computeKs(fun1, fun2, x1, x2, h);
      
-    x1 = x1 + h * (k1(1) + 2 * k1(2) + 2 * k1(3) + k1(4)) / 6;
-    x2 = x2 + h * (k2(1) + 2 * k2(2) + 2 * k2(3) + k2(4)) / 6;
+    tmp1 = x1 + h * (k1(1) + 2 * k1(2) + 2 * k1(3) + k1(4)) / 6;
+    tmp2 = x2 + h * (k2(1) + 2 * k2(2) + 2 * k2(3) + k2(4)) / 6;
     
     %second half-step
-    [k1, k2] = computeKs(fun1, fun2, x1, x2, h);
+    [k1, k2] = computeKs(fun1, fun2, tmp1, tmp2, h);
     
-    tempx12 = x1 + h * (k1(1) + 2 * k1(2) + 2 * k1(3) + k1(4)) / 6;
-    tempx22 = x2 + h * (k2(1) + 2 * k2(2) + 2 * k2(3) + k2(4)) / 6;
-    
-    errors(i + 1, 1) = (tempx12 - x1) / 15;
-    errors(i + 1, 2) = (tempx22 - x2) / 15;
+    tmp1 = tmp1 + h * (k1(1) + 2 * k1(2) + 2 * k1(3) + k1(4)) / 6;
+    tmp2 = tmp2 + h * (k2(1) + 2 * k2(2) + 2 * k2(3) + k2(4)) / 6;
     
     h = 2 * h;
     
-    eps(1) = abs(x1) * epsr + epsa;
-    eps(2) = abs(x2) * epsr + epsa;
+    errors(i, 1) = (tmp1 - x1) / 15;
+    errors(i, 2) = (tmp2 - x2) / 15;
     
-    alpha1 = (eps(1) / abs(errors(i+1, 1)*(h^5)))^(1/5);
-    alpha2 = (eps(2) / abs(errors(i+1, 2)*(h^5)))^(1/5);
+    eps1 = abs(tmp1) * epsr + epsa;
+    eps2 = abs(tmp2) * epsr + epsa;
+    
+    alpha1 = (eps1 / abs(errors(i, 1)))^(1/5);
+    alpha2 = (eps2 / abs(errors(i, 2)))^(1/5);
     
     alpha = min(alpha1, alpha2);
     
     hnew = 0.9 * alpha * h;
     
     if (0.9 * alpha >= 1)
-        if (a + hnew >= 20)
+        if (a + h >= 20)
             break;
         else
             a = a + h;
-            h = min([hnew, 5*h, 20-a]);
-            i = i+1;
+            h = min([hnew, 5 * h, 20 - a]);
+            i = i + 1;
+            steps(i) = h;
+            continue;
         end
     else
-        if (hnew < h)
-            h = hnew;
-        else
+        if (hnew < 0)
             error('Cant solve with this epsilon');
+        else
+            h = hnew;
         end
     end
+   
+    i = i + 1;
+    steps(i) = h;
 end
--- Project C No 15/Task 2/Task2.m
+++ Project C No 15/Task 2/Task2.m
@@ -2,7 +2,7 @@
 derX1 = @(x1, x2) x2 + x1 * (0.5 - x1 * x1 - x2 * x2);
 derX2 = @(x1, x2) -x1 + x2 * (0.5 - x1 * x1 - x2 * x2);
 
-step = 1;
+step = 0.0001;
 
 % RK4 solution
 % [x1, x2] = RK4(derX1, derX2, step, 0, 0.3);
@@ -19,30 +19,48 @@ step = 1;
 % xlabel('x1'); ylabel('x2');
 % grid on;
 % saveas(gcf, "ada", 'jpg');
-% 
-% [n m] = size(x1);
-% plot(0:(m-1), x1, 'g')
-% title(['Adams method with constant step size = ', num2str(step), ' solution versus time']);
-% xlabel('t'); ylabel('x1 - green, x2 - red');
-% grid on;
-% hold on;
-% [n m] = size(x2);
-% plot(0:(m-1), x2, 'r')
-% 
-% saveas(gcf, "adat", 'jpg');
 
 % ode build-in Matlab function
 % hold on;
 % [t, y] = ode45(@vdp1, [0 20], [0; 0.3]);
 % plot(y(:,1), y(:,2), 'r');
 % hold on
+% saveas(gcf, "adaode", 'jpg');
 
 % RK4 variable step size
-[x1, x2] = RK4variable(derX1, derX2, 0, 0.3, 0.5, 10^-10, 10^-10);
-plot(x1, x2, 'b');
-title(['RK4 method with variable step size, initial h = ', num2str(step), ' compared with ode45']);
-xlabel('x1'); ylabel('x2');
-grid on;
+% [x1, x2, errors, steps] = RK4variable(derX1, derX2, 0, 0.3, step, 10^-4, 10^-4);
+% plot(x1, x2, 'b');
+% title(['RK4 method with variable step size, initial h = ', num2str(step)]);
+% xlabel('x1'); ylabel('x2');
+% grid on;
+% saveas(gcf, "rk4var", 'jpg');
+% 
+% [n m] = size(x1);
+% plot(0:(m-1), x1, 'g')
+% title(['RK4 method with variable step size solution versus time']);
+% xlabel('t'); ylabel('x1 - green, x2 - red');
+% grid on;
+% hold on;
+% [n m] = size(x2);
+% plot(0:(m-1), x2, 'r')
+% saveas(gcf, "rk4t", 'jpg');
+
+% [n m] = size(steps);
+% plot(0:(m-1), steps, 'm')
+% title(['RK4 method with variable step size steps versus time']);
+% xlabel('t'); ylabel('step');
+% grid on;
+% saveas(gcf, "stept", 'jpg');
+% 
+% [n m] = size(errors);
+% plot(0:(n-1), errors(:, 1), 'k')
+% title(['RK4 method with variable step size error versus time']);
+% xlabel('t'); ylabel('x1 - black, x2 - red');
+% grid on;
+% hold on;
+% plot(0:(n-1), errors(:, 2), 'r')
+% saveas(gcf, "rk4err", 'jpg');
+
 
 
 
